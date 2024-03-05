7fe988d4dabd7a7ec4a65fa45984bbed599ca8d4
Miguel Beckers <52552717+miguelbeckers@users.noreply.github.com>
2024-03-05 11:07:07 +0000

    updated DTOs

---
+++ src/dtos/classroomDto.ts
@@ -0,0 +1,8 @@
+interface ClassroomDto {
+    id: number;
+    name: string;
+    abbreviation: string;
+    unavailabilityIds: number[];
+    classroomResourceIds: number[];
+    classroomTypeId: number | null;
+}
